c05f69528a7e83c142fad03c1d0bedc489511441
maxgardner <max.c.gardner@gmail.com>
2018-01-23 12:10:44 -0600

    Pull username out into a variable; write results to a file.

---
+++ gitlic.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+// you need to generate personal access token at
+// https://github.com/settings/applications#personal-access-tokens
+// Put your personal access token in pat.txt
+
+type userdata struct {
+	name string
+	pat  string
+}
+
+func setUser() userdata {
+	fh, err := ioutil.ReadFile("user.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	userInfo := strings.Split(string(fh), ",")
+	user := userdata{name: userInfo[0], pat: userInfo[1]}
+	// fmt.Printf("Retrieved user info: %s\n", user)
+	return user
+}
+
+func main() {
+	ctx := context.Background()
+	user := setUser()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: user.pat,
+		},
+	)
+	authClient := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(authClient)
+
+	orgs, _, err := client.Organizations.List(ctx, user.name, nil)
+
+	if err == nil {
+		fmt.Print("Working...\n")
+	}
+
+	var buffer bytes.Buffer
+
+	for _, org := range orgs {
+		if err != nil {
+			fmt.Printf("Failed with %s\n", err)
+			return
+		}
+		buffer.WriteString(fmt.Sprint("Org: ", *org.Login, "\n"))
+
+		repos, _, err := client.Repositories.List(ctx, *org.Login, nil)
+		if err != nil {
+			fmt.Printf("Failed with %s\n", err)
+			return
+		}
+		for _, repo := range repos {
+			//fmt.Printf("Repo: %s \n", *repo.Owner.Login)
+			lics, _, err := client.Repositories.License(ctx, *repo.Owner.Login, *repo.Name)
+			if err == nil {
+				// fmt.Printf("  Repo: %s [%s] \n", *repo.Name, *lics.License.Name)
+				buffer.WriteString(fmt.Sprint("  Repo: ", *repo.Name, " [", *lics.License.Name, "]\n"))
+			}
+		}
+	}
+
+	ioutil.WriteFile("list.txt", []byte(buffer.String()), 0644)
+
+	if err != nil {
+		fmt.Printf("Failed to write to file with %s\n", err)
+	} else {
+		fmt.Print("List now available in list.txt\n")
+	}
+}
