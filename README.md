ba66946fa07ee3e2589794c3062d6af9d1c0661f
maxgardner <max.c.gardner@gmail.com>
2018-03-05 17:21:52 -0600

    Handle pagination; improve progress checker

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 A small program to generate CSV reports on the GitHub repositories for organizations of which you are a member.
 
 ## Usage
-You'll need to generate personal access token [in your settings](https://github.com/settings/applications#personal-access-tokens). Put your username and personal access token, separated by a comma, in "auth.txt", which will be read into the tool. Example:
+You'll need to generate a personal access token [in your settings](https://github.com/settings/applications#personal-access-tokens). If you want to include private repositories in your reports, be sure to select the entire repo scope in the token settings. Then, put your username and personal access token, separated by a comma, in "auth.txt", which will be read into the tool. Example:
 
 `maxgardner,PAT`
+
+### Ignoring repositories
+If you know specific repositories you aren't interested in seeing data from, include their names separated by a comma in a file called "ignore.txt", which will be read into the tool.
 
 ### Sample Output
 _repos.csv_
--- gitlic-check.go
+++ gitlic-check.go
@@ -5,15 +5,14 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
-// you need to generate personal access token at
-// https://github.com/settings/applications#personal-access-tokens
-// Put your personal access token in pat.txt
+// Follow instructions in README for proper authentication
 
 type userdata struct {
 	name string
@@ -31,9 +30,41 @@ func setUser() userdata {
 	return user
 }
 
+func getIgnoredOrgs() map[string]bool {
+	fh, err := ioutil.ReadFile("ignore.txt")
+	if err != nil {
+		return nil
+	}
+	orgNames := strings.Split(string(fh), ",")
+	ignoredOrgs := make(map[string]bool)
+	for _, org := range orgNames {
+		ignoredOrgs[org] = true
+	}
+	return ignoredOrgs
+}
+
+func saveToFile(name string, data string) error {
+	// From Go docs
+	// If the file doesn't exist, create it, or append to the file
+	f, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	f.WriteString(data)
+	if err != nil {
+		return err
+	}
+	f.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	ctx := context.Background()
 	user := setUser()
+	ignoredOrgs := getIgnoredOrgs()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{
 			AccessToken: user.pat,
@@ -48,24 +79,69 @@ func main() {
 		return
 	}
 
-	fmt.Print("Working...\n")
+	fmt.Print("Working... This may take a minute or two.\n")
+
+	os.Remove("repos.csv")
+	os.Remove("users.csv")
+
 	var repoBuffer bytes.Buffer
 	var userBuffer bytes.Buffer
 
-	repoBuffer.WriteString("Org, Repo, License\n")
-	userBuffer.WriteString("Org, User, Two-Factor Enabled\n")
+	repoBuffer.WriteString("Org,Repo,Private,Fork,License\n")
+	if err != nil {
+		fmt.Printf("Failed to write to repo buffer with %s\n", err)
+	}
+	userBuffer.WriteString("Org,User,Two-Factor Enabled\n")
+	if err != nil {
+		fmt.Printf("Failed to write to user buffer with %s\n", err)
+	}
 
-	for _, org := range orgs {
+	saveToFile("repos.csv", repoBuffer.String())
+	if err != nil {
+		fmt.Printf("Initial save to repos CSV failed with %s\n", err)
+	}
 
-		repos, _, err := client.Repositories.List(ctx, *org.Login, nil)
-		if err != nil {
-			fmt.Printf("Repositories.List failed with %s\n", err)
-			return
+	saveToFile("users.csv", userBuffer.String())
+	if err != nil {
+		fmt.Printf("Initial save to users CSV failed with %s\n", err)
+	}
+
+	repoBuffer.Reset()
+	userBuffer.Reset()
+
+	for i, org := range orgs {
+		if ignoredOrgs != nil && ignoredOrgs[*org.Login] {
+			fmt.Printf("Ignored %s, %d of %d\n", *org.Login, i+1, len(orgs))
+			continue
+		}
+
+		var repos []*github.Repository
+		opt := &github.RepositoryListByOrgOptions{
+			ListOptions: github.ListOptions{PerPage: 100},
+		}
+
+		for {
+			partialRepos, resp, err := client.Repositories.ListByOrg(ctx, *org.Login, opt)
+			if err != nil {
+				fmt.Printf("Repositories.List failed with %s\n", err)
+				return
+			}
+
+			repos = append(repos, partialRepos...)
+
+			if resp.NextPage == 0 {
+				opt.Page = 1
+				break
+			}
+			opt.Page = resp.NextPage
 		}
 
 		for _, repo := range repos {
 			// fmt.Printf("  Repo: %s [%s] \n", *repo.Name, *lics.License.Name)
-			repoBuffer.WriteString(fmt.Sprint(*org.Login, ",", *repo.Name, ","))
+			repoBuffer.WriteString(fmt.Sprint(*org.Login, ",", *repo.Name, ",", *repo.Private, ",", *repo.Fork, ","))
+			if err != nil {
+				fmt.Printf("Failed to write to repo buffer on %s with %s\n", *org.Login, err)
+			}
 
 			lics, _, err := client.Repositories.License(ctx, *repo.Owner.Login, *repo.Name)
 			if err != nil {
@@ -73,37 +149,92 @@ func main() {
 				continue
 			}
 			repoBuffer.WriteString(fmt.Sprint(*lics.License.Name, "\n"))
+			if err != nil {
+				fmt.Printf("Failed to write to repo buffer on %s with %s\n", *org.Login, err)
+			}
 		}
 
-		members, _, err := client.Organizations.ListMembers(ctx, *org.Login, nil)
-		if err != nil {
-			fmt.Printf("Organizations.ListMembers, no filter, failed with %s\n", err)
-			continue
+		var members []*github.User
+		memOpt := &github.ListMembersOptions{
+			ListOptions: github.ListOptions{PerPage: 100},
 		}
 
-		membersNo2F, _, err := client.Organizations.ListMembers(ctx, *org.Login, &github.ListMembersOptions{Filter: "2fa_disabled"})
-		if err != nil {
-			fmt.Printf("Organizations.ListMembers, 2FA filter, failed with %s\n", err)
-			continue
+		var membersNo2f []*github.User
+		no2fOpt := &github.ListMembersOptions{
+			Filter:      "2fa_disabled",
+			ListOptions: github.ListOptions{PerPage: 100},
+		}
+
+		for {
+			partialMembers, resp, err := client.Organizations.ListMembers(ctx, *org.Login, memOpt)
+			if err != nil {
+				fmt.Printf("Organizations.ListMembers, no filter, failed with %s\n", err)
+				break
+			}
+
+			members = append(members, partialMembers...)
+
+			if resp.NextPage == 0 {
+				memOpt.Page = 1
+				break
+			}
+			memOpt.Page = resp.NextPage
+		}
+
+		for {
+			partialNo2f, resp, err := client.Organizations.ListMembers(ctx, *org.Login, no2fOpt)
+			if err != nil {
+				fmt.Printf("Organizations.ListMembers, 2FA filter, failed with %s\n", err)
+				break
+			}
+
+			membersNo2f = append(membersNo2f, partialNo2f...)
+
+			if resp.NextPage == 0 {
+				no2fOpt.Page = 1
+				break
+			}
+			no2fOpt.Page = resp.NextPage
 		}
 
 		membersFilter := make(map[string]bool)
-		for _, member := range membersNo2F {
+		for _, member := range membersNo2f {
 			membersFilter[*member.Login] = true
 		}
 
 		for _, member := range members {
 			userBuffer.WriteString(fmt.Sprint(*org.Login, ",", *member.Login, ","))
+			if err != nil {
+				fmt.Printf("Failed to write to user buffer on %s with %s\n", *org.Login, err)
+			}
+
 			if membersFilter[*member.Login] {
 				userBuffer.WriteString("False\n")
+				if err != nil {
+					fmt.Printf("Failed to write to user buffer on %s with %s\n", *org.Login, err)
+				}
 				continue
 			}
 			userBuffer.WriteString("True\n")
+			if err != nil {
+				fmt.Printf("Failed to write to user buffer on %s with %s\n", *org.Login, err)
+			}
 		}
-	}
 
-	ioutil.WriteFile("repos.csv", []byte(repoBuffer.String()), 0644)
-	ioutil.WriteFile("users.csv", []byte(userBuffer.String()), 0644)
+		saveToFile("repos.csv", repoBuffer.String())
+		if err != nil {
+			fmt.Printf("Saving to repos CSV failed on %d with %s\n", i, err)
+		}
+
+		saveToFile("users.csv", userBuffer.String())
+		if err != nil {
+			fmt.Printf("Saving to users CSV failed on %d with %s\n", i, err)
+		}
+
+		repoBuffer.Reset()
+		userBuffer.Reset()
+		fmt.Printf("Completed %d of %d\n", i+1, len(orgs))
+	}
 
 	fmt.Print("CSVs are now ready\n")
 }
