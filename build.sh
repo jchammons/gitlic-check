d372a534d48b81ac2ef154ee90ad3a5528ec2a16
Alex Seubert <alexseubert@gmail.com>
2018-10-02 17:28:50 -0500

    Add augit to build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ echo "Building container image"
 echo "Computed tag: $tag"
 docker build --no-cache -t quay.io/solarwinds/gitlic-check . && \
 docker tag quay.io/solarwinds/gitlic-check quay.io/solarwinds/gitlic-check:$tag && \
+docker build --no-cache -t quay.io/solarwinds/augit-server:$tag -f Dockerfile_augit .
 echo "Login to quay" && \
 docker login -u $DOCKER_USER -p $DOCKER_PASS quay.io && \
 echo "Login succeeded. Pushing images" && \
 docker push quay.io/solarwinds/gitlic-check && \
 docker push quay.io/solarwinds/gitlic-check:$tag && \
+docker push quay.io/solarwinds/augit-server:$tag
 deploy="false" && \
 echo "Branch name: $CIRCLE_BRANCH" && \
 case $CIRCLE_BRANCH in
@@ -29,6 +31,17 @@ if [[ $deploy = "true" ]]; then
     ./kubectl -n solarwindsio set image cronjob gitlic-check-cron gitlic-check=quay.io/solarwinds/gitlic-check:$tag && \
     sleep 5 && \
     response=`./kubectl -n solarwindsio rollout status cronjob/gitlic-check-cron --watch=true` && \
+    if [[ $response = *"error"* ]]; then
+        echo "Deployment not successful with msg: '$response'. Rolling back. . . "
+        ./kubectl rollout undo cronjob/gitlic-check-cron
+        echo "Rolling back done . . . "
+        exit 1
+    fi
+    ./kubectl -n solarwindsio set image cronjob augit-gh-report augit-gh-report=quay.io/solarwinds/augit-server:$tag && \
+    ./kubectl -n solarwindsio set image cronjob augit-populator augit-populator=quay.io/solarwinds/augit-server:$tag && \
+    ./kubectl -n solarwindsio set image deployment augit augit-server=quay.io/solarwinds/augit-server:$tag && \
+    sleep 5 && \
+    response=`./kubectl -n solarwindsio rollout status deployments/augit --watch=true` && \
     if [[ $response = *"error"* ]]; then
         echo "Deployment not successful with msg: '$response'. Rolling back. . . "
         ./kubectl rollout undo cronjob/gitlic-check-cron
