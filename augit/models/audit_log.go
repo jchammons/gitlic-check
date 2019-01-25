e9f8cad7dd4a301cc897a5be5b42c12adfa0a51f
Alex Seubert <alexseubert@gmail.com>
2019-01-25 17:00:32 -0600

    Add audit log model

---
+++ augit/models/audit_log.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+)
+
+type AuditLog struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	GithubID  string    `json:"github_id" db:"github_id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+}
+
+// String is not required by pop and may be deleted
+func (a AuditLog) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// AuditLogs is not required by pop and may be deleted
+type AuditLogs []AuditLog
+
+// String is not required by pop and may be deleted
+func (a AuditLogs) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (a *AuditLog) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: a.GithubID, Name: "GithubID"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (a *AuditLog) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (a *AuditLog) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
