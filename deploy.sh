a975a20d1fd0256256bf38642f20ea3d170ac3fa
Alex Seubert <alexseubert@gmail.com>
2019-01-30 10:19:05 -0600

    Add offboard cron to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ if [[ $deploy = "true" ]]; then
     kubectl -n solarwindsio set image cronjob gitlic-check-cron gitlic-check=quay.io/solarwinds/gitlic-check:$tag
     kubectl -n solarwindsio set image cronjob augit-gh-report augit-gh-report=quay.io/solarwinds/augit-server:$tag
     kubectl -n solarwindsio set image cronjob augit-populator augit-populator=quay.io/solarwinds/augit-server:$tag
+    kubectl -n solarwindsio set image cronjob augit-gh-offboard-cron augit-populator=quay.io/solarwinds/augit-server:$tag
     kubectl -n solarwindsio set image deployment augit augit-server=quay.io/solarwinds/augit-server:$tag
     sleep 5
     response=`kubectl -n solarwindsio rollout status deployments/augit --watch=true`
